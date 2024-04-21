f9eebbe76f0691752f284fbaed0cd3c6d38af70b
Krisna Lach <lach.krisna@gmail.com>
2024-04-21 16:58:13 -0400

    Navigation work + prettier

--- src/client/db.js
+++ src/client/db.js
@@ -1,107 +1,102 @@
 let db = null;
-db = new PouchDB('mydb');
+db = new PouchDB("mydb");
 
 const users = {
-    'john': {
-        'winrate': 1,
-        'highest_bid': 10000,
-        'all_in_cnt': 50,
-        'blackjacks': 17,
-    },
-    'phil': {
-        'winrate': .7,
-        'highest_bid': 3000,
-        'all_in_cnt': 3,
-        'blackjacks': 13,
-    },
-    'stu': {
-        'winrate': .2,
-        'highest_bid': 70,
-        'all_in_cnt': 1000,
-        'blackjacks': 1,
-    },
+  john: {
+    winrate: 1,
+    highest_bid: 10000,
+    all_in_cnt: 50,
+    blackjacks: 17,
+  },
+  phil: {
+    winrate: 0.7,
+    highest_bid: 3000,
+    all_in_cnt: 3,
+    blackjacks: 13,
+  },
+  stu: {
+    winrate: 0.2,
+    highest_bid: 70,
+    all_in_cnt: 1000,
+    blackjacks: 1,
+  },
 };
 
 /**
  * Initialize the database
  */
 async function init() {
-    try {
-        await db.put({
-            _id: 'lb',
-            'leaderboard': [],
-        });
-        await db.put();
-    }
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    await db.put({
+      _id: "lb",
+      leaderboard: [],
+    });
+    await db.put();
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 /**
  * (INFO) This should probably be done automatically
  * on new account creation / first game played
- * @param {*} obj 
+ * @param {*} obj
  */
 export async function addToLeaderboard(obj) {
-    try {
-        let lb = await db.get('lb');
-        lb['leaderboard'].push(obj);
-        await db.put({
-            _id: 'lb',
-            'leaderboard': lb,
-        });
-    }
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    let lb = await db.get("lb");
+    lb["leaderboard"].push(obj);
+    await db.put({
+      _id: "lb",
+      leaderboard: lb,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 /**
- * 
+ *
  */
 export async function getLeaderboard() {
-    try {
-        const response = await db.get('lb');
-        // do we need to do any parsing? 
-        return response;
-    }
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    const response = await db.get("lb");
+    // do we need to do any parsing?
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 /**
- * 
- * @param {string} id 
+ *
+ * @param {string} id
  */
 export async function getUserStats() {
-    try {
-        const userStats = await db.get('currUser');
-        console.log(userStats);
-        // Figure out how we want to represent this
-        return userStats['stats'];
-    }
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    const userStats = await db.get("currUser");
+    console.log(userStats);
+    // Figure out how we want to represent this
+    return userStats["stats"];
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 /**
  * Gets the current user that is logged in.
  * Assumes only one username is stored in local storage
- * at a time. 
+ * at a time.
  * @returns - the username of the currently logged in user, -1 otherwise
  */
 export async function getUser() {
-    try {
-        const response = await db.get('currUser');
-        return response['name'];
-    }
-    catch (err) {
-        console.error(err);
-        return -1;
-    }
+  try {
+    const response = await db.get("currUser");
+    return response["name"];
+  } catch (err) {
+    console.error(err);
+    return -1;
+  }
 }
 
 /**
@@ -110,41 +105,38 @@ export async function getUser() {
  * @param {string} name
  */
 export async function login(name) {
-    try {
-        const response = await db.put({
-            _id: 'currUser',
-            'name': name,
-            'stats': users[name],
-        });
-    }
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    const response = await db.put({
+      _id: "currUser",
+      name: name,
+      stats: users[name],
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 /**
  * Log out a user by removing their username from
  * local storage
- * @param {string} name 
+ * @param {string} name
  */
 export async function logout() {
-    try {
-        const response = await db.get('currUser');
-        await db.remove(response);
-        return -1;
-    }
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    const response = await db.get("currUser");
+    await db.remove(response);
+    return -1;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 // Prevent re-initializing the mock database
 try {
-    // See if it is already pre-populated
-    const temp = await db.get('lb');
-}
-catch (err) {
-    // If not, initialize it
-    await init();
-    console.error(err)
+  // See if it is already pre-populated
+  const temp = await db.get("lb");
+} catch (err) {
+  // If not, initialize it
+  await init();
+  console.error(err);
 }
--- src/client/script.js
+++ src/client/script.js
@@ -1,85 +1,108 @@
 "use strict";
-
 import * as db from "./db.js";
 
+document.addEventListener("DOMContentLoaded", (event) => {
+  function navigate(viewId) {
+    document.querySelectorAll(".view").forEach((view) => {
+      view.style.display = "none";
+    });
+    document.getElementById(viewId).style.display = "block";
+  }
+
+  document.querySelectorAll(".table").forEach((button) => {
+    button.addEventListener("click", (event) => navigate("table-view"));
+  });
+
+  document.querySelectorAll(".stats").forEach((button) => {
+    button.addEventListener("click", (event) => navigate("stats-view"));
+  });
+
+  document.querySelectorAll(".leaderboard").forEach((button) => {
+    button.addEventListener("click", (event) => navigate("lb-view"));
+  });
+
+  document.querySelectorAll(".tutorial").forEach((button) => {
+    button.addEventListener("click", (event) => navigate("tutorial-view"));
+  });
+});
+
 // login elements
-const loginLabel = document.createElement('label');
-const loginInput = document.createElement('input');
-const loginButton = document.createElement('button');
-const loginForm = document.querySelector('#login-form div');
+const loginLabel = document.createElement("label");
+const loginInput = document.createElement("input");
+const loginButton = document.createElement("button");
+const loginForm = document.querySelector("#login-form div");
 
 // Set html attributes and display text
-loginLabel.id = 'username-label';
-loginLabel.htmlFor = 'username';
-loginLabel.innerText = 'Username: ';
-loginInput.id = 'username';
-loginInput.type = 'text';
+loginLabel.id = "username-label";
+loginLabel.htmlFor = "username";
+loginLabel.innerText = "Username: ";
+loginInput.id = "username";
+loginInput.type = "text";
 loginInput.required = true;
-loginButton.id = 'login-button';
-loginButton.innerText = 'Login';
+loginButton.id = "login-button";
+loginButton.innerText = "Login";
 
 // logout elements
-const logoutButton = document.createElement('button');
-const welcomeElem = document.createElement('div');
+const logoutButton = document.createElement("button");
+const welcomeElem = document.createElement("div");
 
 // Attempt to grab current user in session
 let user = await db.getUser();
 
 // Set html attributes
-logoutButton.innerHTML = 'Log Out';
+logoutButton.innerHTML = "Log Out";
 welcomeElem.innerHTML = `Welcome, ${user}!`;
 
 await renderLogin(user);
 
 // Add event listener for login button
-loginButton.addEventListener('click', async (event) => {
-    event.preventDefault();
+loginButton.addEventListener("click", async (event) => {
+  event.preventDefault();
 
-    // update user variable
-    user = loginInput.value;
-    await db.login(user);
-    welcomeElem.innerHTML = `Welcome, ${user}!`;
-    
-    // Remove login elements
-    loginForm.removeChild(loginButton);
-    loginForm.removeChild(loginInput);
-    loginForm.removeChild(loginLabel);
+  // update user variable
+  user = loginInput.value;
+  await db.login(user);
+  welcomeElem.innerHTML = `Welcome, ${user}!`;
 
-    // Add logout elements
-    loginForm.appendChild(welcomeElem);
-    loginForm.appendChild(logoutButton);
+  // Remove login elements
+  loginForm.removeChild(loginButton);
+  loginForm.removeChild(loginInput);
+  loginForm.removeChild(loginLabel);
+
+  // Add logout elements
+  loginForm.appendChild(welcomeElem);
+  loginForm.appendChild(logoutButton);
 });
 
 // Add event listener for logout button
-logoutButton.addEventListener('click', async (event) => {
-    event.preventDefault();
-    
-    user = await db.logout();
+logoutButton.addEventListener("click", async (event) => {
+  event.preventDefault();
 
-    // Remove logout elements
-    loginForm.removeChild(welcomeElem);
-    loginForm.removeChild(logoutButton);
+  user = await db.logout();
 
-    // Add back login elements
-    loginForm.appendChild(loginLabel);
-    loginForm.appendChild(loginInput);
-    loginForm.appendChild(loginButton);
+  // Remove logout elements
+  loginForm.removeChild(welcomeElem);
+  loginForm.removeChild(logoutButton);
+
+  // Add back login elements
+  loginForm.appendChild(loginLabel);
+  loginForm.appendChild(loginInput);
+  loginForm.appendChild(loginButton);
 });
 
 /**
  * Checks to see if the user's session exists in
  * local storage. If so, render the logout UI, if not,
  * render the login UI.
- * @param {string} user 
+ * @param {string} user
  */
 async function renderLogin(user) {
-    if (user === -1) {
-        loginForm.appendChild(loginLabel);
-        loginForm.appendChild(loginInput);
-        loginForm.appendChild(loginButton);
-    }
-    else {
-        loginForm.appendChild(welcomeElem);
-        loginForm.appendChild(logoutButton);
-    }
+  if (user === -1) {
+    loginForm.appendChild(loginLabel);
+    loginForm.appendChild(loginInput);
+    loginForm.appendChild(loginButton);
+  } else {
+    loginForm.appendChild(welcomeElem);
+    loginForm.appendChild(logoutButton);
+  }
 }
