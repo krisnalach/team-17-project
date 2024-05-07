9642b4540793ee15b634418b90313942f34a096e
Trace Lustgarten <178trace@gmail.com>
2024-05-07 18:00:35 -0400

    More Game Logic

--- src/client/cards.js
+++ src/client/cards.js
@@ -8,34 +8,70 @@
 // 13 ranks: 2-10, J, Q, K, A
 // 52 cards total
 
-var suits = ["hearts", "diamonds", "clubs", "spades"];
-var ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, [1, 11]];
+let suits = ["hearts", "diamonds", "clubs", "spades"];
+let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, [1, 11]];
 
-var deck = [];
-var playerHand = [];
-var dealerHand = [];
-var discardedCards = [];
+let hearts = 5; 
+let heart1 = document.getElementById("heart1");
+let heart2 = document.getElementById("heart2");
+let heart3 = document.getElementById("heart3");
+let heart4 = document.getElementById("heart4");
+let heart5 = document.getElementById("heart5");
+
+let deck = [];
+let playerHand = [];
+let playerHTML = document.querySelector(".player");
+let playerScoreHTML = document.querySelector(".player-score");
+let dealerHand = [];
+let dealerHTML = document.querySelector(".dealer");
+let dealerScoreHTML = document.querySelector(".dealer-score");
+let discardedCards = [];
+
+let score = 0;
 
 // the createDeck function creates and returns a shuffled deck of cards
 function createDeck() {
-  for (var i = 0; i < suits.length; i++) {
-    for (var j = 0; j < ranks.length; j++) {
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < ranks.length; j++) {
       deck.push({ suit: suits[i], rank: ranks[j], value: values[j] });
     }
   }
   // shuffle the deck
-  for (var i = 0; i < deck.length; i++) {
-    var randomIndex = Math.floor(Math.random() * deck.length);
-    var temp = deck[i];
+  for (let i = 0; i < deck.length; i++) {
+    let randomIndex = Math.floor(Math.random() * deck.length);
+    let temp = deck[i];
     deck[i] = deck[randomIndex];
     deck[randomIndex] = temp;
   }
   return deck;
 }
 
+function newRound() {
+  // discard previous hands
+  discardHands();
+  // deal initial hands
+  dealInitialHands();
+  // update player score
+  playerScoreHTML.textContent = calculateHandValue(playerHand);
+  // update dealer score
+  dealerScoreHTML.textContent = calculateHandValue(dealerHand);
+}  
+
 // the drawCard function returns the top card from the deck
 function drawCard(deck) {
+  // if deck is less than 8 cards, shuffle the discarded cards back into the deck
+  if (deck.length < 8) {
+    deck = deck.concat(discardedCards);
+    discardedCards = [];
+    // shuffle the deck
+    for (let i = 0; i < deck.length; i++) {
+      let randomIndex = Math.floor(Math.random() * deck.length);
+      let temp = deck[i];
+      deck[i] = deck[randomIndex];
+      deck[randomIndex] = temp;
+    }
+  }
   return deck.pop();
 }
 
@@ -56,14 +92,36 @@ function discardHands() {
 
 // the playerHit function adds a card to the player's hand
 function playerHit() {
-  playerHand.push(drawCard(deck));
+  card_drawn = drawCard(deck);
+  playerHand.push(card_drawn);
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.textContent = card_drawn.rank;
+  playerHTML.appendChild(card);
+  // update player score
+  playerScoreHTML.textContent = calculateHandValue(playerHand);
+  // check if the player has busted
+  if (calculateHandValue(playerHand) > 21) {
+    bustPlayer()
+  }
 }
 
-// TODO the doubleDown function doubles the player's hearts risked and adds one card to the player's hand
+// the doubleDown function doubles the player's hearts risked and adds one card to the player's hand
 function doubleDown() {
   // double the player's hearts risked
   playerHand.push(drawCard(deck));
-  // make player stand
+  // add htmly element
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.textContent = card_drawn.rank;
+  playerHTML.appendChild(card);
+  // update player score
+  playerScoreHTML.textContent = calculateHandValue(playerHand);
+  // check if the player has busted
+  if (calculateHandValue(playerHand) > 21) {
+    bustPlayer()
+  }
+  stand();
 }
 
 // the dealerHit function adds a card to the dealer's hand
@@ -75,5 +133,58 @@ function dealerHit() {
 function split() {
   // check if the player has two cards of the same rank
   // if they do, split the hand into two hands
+  return false;
+}
+
+// TODO stand function that runs the dealer's turn and then determines the winner
+function stand() {
+  // dealer hits until their hand value is 17 or greater
+  while (calculateHandValue(dealerHand) < 17) {
+    dealerHit();
+  }
+  if (calculateHandValue(dealerHand) > 21) {
+    bustDealer();
+  }
+  // determine the winner
+  if (calculateHandValue(playerHand) > calculateHandValue(dealerHand)) {
+    score += 1;
+    // TODO add a you win popup
+    newRound();
+  } else if (calculateHandValue(playerHand) < calculateHandValue(dealerHand)) {
+    // lose a heart
+  } else {
+    // TODO tie popup
+    newRound();
+  }
+  
+}
+
+function loseHeart() {
+  if (hearts === 5) {
+    heart5.src = "images/PHeart-Empty.png";
+  }
+  else if (hearts === 4) {
+    heart4.src = "images/PHeart-Empty.png";
+  }
+  else if (hearts === 3) {
+    heart3.src = "images/PHeart-Empty.png";
+  }
+  else if (hearts === 2) {
+    heart2.src = "images/PHeart-Empty.png";
+  }
+  else if (hearts === 1) {
+    heart1.src = "images/PHeart-Empty.png";
+    // TODO game over function
+  }
+  hearts -= 1;
+}
+
+// TODO popup saying bust, then lose a heart
+function bustPlayer() {
+  return false;
+}
+
+// TODO popup saying bust, then win
+function bustDealer() {
   return false;
 }
